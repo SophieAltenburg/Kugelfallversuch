582351a7d379075cca3977e2df4a821e6874a5e0
jahampel <jakobhampel98@gmx.de>
2020-07-31 10:58:38 +0200

    Plot acceptable decleration

--- laborbericht/plot_deceleration_function.py
+++ laborbericht/plot_deceleration_function.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 30 14:53:09 2020
-
-@author: jakob
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+with open("../protokolle/twoCycles.csv", "r") as file:
+    two_cycles = [[int(x) for x in line.split(",")] for line in file.readlines()]
+    two_cycles = np.asarray(two_cycles)
+    
 def f(x):
     if x <= 900:
         return 20
@@ -18,4 +17,17 @@ def f(x):
         return (x-2200)/10+85
     
 y = [f(x) for x in range(2500)]
-plt.plot(y)
+plt.plot(two_cycles[:-3,1],
+         np.diff(two_cycles[:-2,1]),
+         label="Abbremsung ohne Außeneinwirkung")
+plt.plot(y,
+         label="Akzeptierte Abbremsung")
+plt.xlim(min(two_cycles[:-3,1])-100, 2600)
+plt.ylim(0, 120)
+plt.grid(True)
+
+plt.xlabel("$tr$ / ms")
+plt.ylabel("$delta tr$ / ms")
+plt.legend()
+
+plt.savefig("acceptable_deceleration.pdf")
